d677e23f3d2b6cb939f5b14de6e2c69ac25a9986
jazminkneup <jazmin.kneup@gmail.com>
2024-06-19 18:47:27 -0400

    Primer Reaact con JSX

---
+++ primer-react-jsx/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Lista from './lista';
+
+function App() {
+
+    return (
+      <>
+        <h1> Hello Dojo</h1>
+        <h2>Things I need to do:</h2>
+        <Lista/>
+        </>
+    ); 
+  }
+export default App;
